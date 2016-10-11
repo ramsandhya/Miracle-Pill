540151de26918e9692e7f601f0ad9160afc0234b
ramsandhya <ram.sandhya@gmail.com>
2016-10-11 14:36:57 -0400

    State Picker Configuration

--- MiraclePills/MiraclePills/ViewController.swift
+++ MiraclePills/MiraclePills/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York", "Georgia", "Florida", "Virginia"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
-
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
